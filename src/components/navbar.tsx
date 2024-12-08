b2833fdbb9824175d4ba9d3fbf1826e23cea2500
Sanan Maarouf <sanan.adnan97@gmail.com>
2024-12-08 23:46:44 +0100

    booking link on navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,7 +4,9 @@ export default function Navbar() {
   return (
     <nav className="flex items-center md:justify-center p-4 bg-[#E9EEEC] border-b border-gray-300">
       <header className="text-xl md:text-3xl uppercase text-green-800 font-bold">Le duo du bistro</header>
-      <button className="bg-green-800 absolute right-4 text-lg md:text-xl text-white py-2 px-4 rounded">kontakt</button>
+      <a href="mailto:duo@leduodubistro.no?subject=Booking">
+        <button className="bg-green-800 absolute right-4 text-lg md:text-xl text-white py-2 px-4 rounded">kontakt</button>
+      </a>
     </nav>
   );
 };
